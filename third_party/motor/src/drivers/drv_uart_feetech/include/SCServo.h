3fc40a92887fdcabe24175f327cde2d0d340da68
wangjie <jie.wang@spacemit.com>
2026-05-14 10:41:51 +0800

    fix(control_manipulator): remove third_party/motor

--- third_party/motor/src/drivers/drv_uart_feetech/include/SCServo.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef SCSERVO_H
-#define SCSERVO_H
-
-#include "SCSCL.h"
-#include "SMS_STS.h"
-#include "HLSCL.h"
-
-#endif  // SCSERVO_H
--- third_party/motor/src/drivers/drv_uart_rm/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod controller;
-pub use controller::ReachyMiniMotorController;
-
-#[cfg(feature = "python")]
-pub mod bindings;
-
-pub mod control_loop;
-
-pub mod c_api;
